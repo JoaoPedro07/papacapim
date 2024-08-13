e851d83ab793108b38bd545460b977ba6d0736c2
JoaoPedro07 <37756452+JoaoPedro07@users.noreply.github.com>
2024-08-13 16:16:29 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-// import { Contact } from './components/CaixaTexto';
+import {StyleSheet} from 'react-native';
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Contatos from './src/pages/contatos';
 import AddContatos from './src/pages/addcontato';
+import Login from './src/pages/login';
+
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
   <NavigationContainer>
     <Stack.Navigator>
+      <Stack.Screen name='login' component={Login} options={{title:"Login"}}/>
       <Stack.Screen name='Contatos' component={Contatos} options={{title:"Contatos"}}/>
-      <Stack.Screen name="addContatos" component={AddContatos} options={{title:"Adicionar"}}/>
+      <Stack.Screen name='addContatos' component={AddContatos} options={{title:"Adicionar"}}/>
       
     </Stack.Navigator>
   </NavigationContainer>

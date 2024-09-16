8c6edc4e9686c8795d1246a30d7f8f258d0ed6a7
Pedro <jpferreirafsa@gmail.com>
2024-09-15 21:09:37 -0300

    cadastro

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,33 +1,97 @@
 import {Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useState } from "react";
-import { Alert } from 'react-native';
+import { useForm, Controller } from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as yup from "yup";
+
 
 export default function Cadastrar({ navigation }) {
   const [nome, setNome] = useState('');
   const [telefone, setTelefone] = useState('');
   const [senha, setSenha] = useState('');
-  function alerta(){
-    Alert.alert(`${nome} tem o telefone ${telefone}`)
+  const schema = yup.object({
+    name: yup.string().required("Campo obrigatório"),
+    telefone: yup.string().required("Campo obrigatório").min(11, "Mínimo 11 caracteres"),
+    password: yup.string().required("Campo obrigatório").min(6, "Mínimo 6 caracteres"),
+  })
+  const {  
+    handleSubmit, 
+    register, 
+    control, 
+    setValue, 
+    formState: { errors },
+  } = useForm({
+    resolver:yupResolver(schema),
+  });
+
+  const save = (data) => {
+    console.log(data)
   }
-  return (
+  return ( 
     <View style={styles.container}> 
         <Text style={styles.texto}>Usuário</Text>
         <View style={styles.input}>
-            <TextInput placeholder='nome' onChangeText={setNome}/>
+          <Controller 
+          name="name" 
+          control={control} 
+          render={({field: {onChange, value}}) => (
+            <TextInput 
+            name="name" 
+            placeholder='nome' 
+            onChangeText={(value) => onChange(value)}/>
+          )}/>
+        
         </View>
+        {errors?.name && <Text style={{color:"red"}}>{errors?.name?.message}</Text> }
+
         <Text style={styles.texto}>Telefone</Text>
         <View style={styles.input}>
-            <TextInput keyboardType='numeric' placeholder='telefone' onChangeText={setTelefone}></TextInput>
+          <Controller 
+          name="telefone" 
+          control={control} 
+          render={({field: {onChange, value}}) => (
+            <TextInput 
+            keyboardType="numeric" 
+            name="telefone" 
+            placeholder='telefone' 
+            onChangeText={(value) => onChange(value)}/>
+          )}/>
+        
         </View>
+        {errors?.telefone && <Text style={{color:"red"}}>{errors?.telefone?.message}</Text> }
+
         <Text style={styles.texto}>Senha</Text>
         <View style={styles.input}>
-            <TextInput placeholder='senha' onChangeText={setSenha}></TextInput>
+          <Controller 
+          name="password" 
+          control={control} 
+          render={({field: {onChange, value}}) => (
+            <TextInput 
+            name="password" 
+            secureTextEntry={true}
+            placeholder='senha' 
+            onChangeText={(value) => onChange(value)}/>
+          )}/>
+        
         </View>
-        <Button title='Cadastrar' onPress={() => navigation.navigate("Login")}></Button>
+        {errors?.password && <Text style={{color:"red"}}>{errors?.password?.message}</Text> }
+
+        <Button title='Cadastrar' onPress={handleSubmit(save)}></Button>
     </View>
 
   );
-}
+} 
+
+
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   texto:{
